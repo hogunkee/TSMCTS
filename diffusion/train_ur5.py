f2070b4510debc5936034bed0a1493a4eebee2ce
hogunkee <hogunhogun@naver.com>
2023-08-01 14:26:03 +0900

    gif modified

--- diffusion/train_ur5.py
+++ diffusion/train_ur5.py
@@ -30,6 +30,10 @@ import numpy as np
 
 from data_loader import UR5Dataset
 
+def clip_image(im):
+    im_clip = np.clip(im, 0, 1)
+    return im_clip
+
 def normalize_image(im):
     v_max = im.max()
     v_min = im.min()
@@ -320,15 +324,15 @@ class DDPM(nn.Module):
 def train_mnist():
 
     # hardcoding these here
-    n_epoch = 20
+    n_epoch = 30
     batch_size = 64 #256
     n_T = 400 # 500
     device = "cuda:0"
     n_classes = 2
     n_feat = 64 #128 # 128 ok, 256 better (but slower)
-    lrate = 1e-4
+    lrate = 2e-5 #1e-4
     save_model = True #False
-    save_dir = './data/ur5_outputs/'
+    save_dir = './data/ur5_outputs2/'
     ws_test = [0.0, 0.5, 2.0] # strength of generative guidance
 
     ddpm = DDPM(nn_model=ContextUnet(in_channels=3, n_feat=n_feat, n_classes=n_classes), betas=(1e-4, 0.02), n_T=n_T, device=device, drop_prob=0.1)
@@ -397,13 +401,15 @@ def train_mnist():
                     def animate_diff(i, x_gen_store):
                         print(f'gif animating frame {i} of {x_gen_store.shape[0]}', end='\r')
                         plots = []
-                        x_gen_norm = normalize_image(x_gen_store)
+                        x_gen_clip = clip_image(x_gen_store)
+                        #x_gen_norm = normalize_image(x_gen_store)
                         for row in range(n_classes):
                             for col in range(int(n_sample/n_classes)):
                                 axs[row, col].clear()
                                 axs[row, col].set_xticks([])
                                 axs[row, col].set_yticks([])
-                                plots.append(axs[row, col].imshow(x_gen_norm[i,(row*n_classes)+col].transpose([1,2,0])))
+                                plots.append(axs[row, col].imshow(x_gen_clip[i,(row*n_classes)+col].transpose([1,2,0])))
+                                #plots.append(axs[row, col].imshow(x_gen_norm[i,(row*n_classes)+col].transpose([1,2,0])))
                         return plots
                     ani = FuncAnimation(fig, animate_diff, fargs=[x_gen_store],  interval=200, blit=False, repeat=True, frames=x_gen_store.shape[0])    
                     ani.save(save_dir + f"gif_ep{ep}_w{w}.gif", dpi=100,writer=PillowWriter(fps=5))
