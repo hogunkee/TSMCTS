4fb9e43776491f5c9e27a17e75b5a18ba9401d52
hogunkee <hogunhogun@naver.com>
2023-09-14 23:40:02 +0900

    load objects from urdf

--- pybullet/collect_scene_data.py
+++ pybullet/collect_scene_data.py
@@ -6,17 +6,69 @@ import subprocess
 import math
 import pybullet as p 
 import numpy as np
+from transform_utils import euler2quat
 
 opt = lambda : None
-opt.nb_objects = 8 #30 #50
-opt.spp = 64 
-opt.width = 96 #500
-opt.height = 96 #500 
+opt.nb_objects = 10 #30 #50
+opt.inscene_objects = 5
+opt.spp = 32 #64 
+opt.width = 500
+opt.height = 500 
 opt.noise = False
 opt.frame_freq = 4 #8
-opt.nb_frames = 50 #300
-opt.outf = 'capture'
+opt.nb_scenes = 2500 #25
+opt.nb_frames = 4
+opt.outf = 'scene_data'
 
+def get_rotation(roll, pitch, yaw):
+    euler = roll, pitch, yaw
+    x, y, z, w = euler2quat(euler)
+    rot = nvisii.normalize(nvisii.quat(w, x, y, z))
+    return rot
+
+def set_object_pose(ids, pos, rot=None):
+    if rot is None:
+        _, rot = p.getBasePositionAndOrientation(ids['pybullet_id'])
+    p.resetBasePositionAndOrientation(ids['pybullet_id'], pos, rot)
+
+    # get the nvisii entity for that object
+    obj_entity = nvisii.entity.get(ids['nvisii_id'])
+    obj_entity.get_transform().set_position(pos)
+
+    if rot is not None:
+        # nvisii quat expects w as the first argument
+        obj_entity.get_transform().set_rotation(rot)
+    return
+
+def sync_object_poses(object_ids):
+    for ids in object_ids:
+        pos, rot = p.getBasePositionAndOrientation(ids['pybullet_id'])
+        obj_entity = nvisii.entity.get(ids['nvisii_id'])
+        obj_entity.get_transform().set_position(pos)
+        obj_entity.get_transform().set_rotation(rot)
+    return
+
+def get_contact_objects():
+    contact_pairs = set()
+    for contact in p.getContactPoints():
+        body_A = contact[1]
+        body_B = contact[2]
+        contact_pairs.add(tuple(sorted((body_A, body_B))))
+    collisions = set()
+    for cp in contact_pairs:
+        if cp[0] == 0:
+            continue
+        collisions.add(cp)
+    return collisions
+
+def get_velocity(object_ids):
+    velocities_linear = []
+    velocities_rotation = []
+    for ids in object_ids:
+        vel_linear, vel_rot = p.getBaseVelocity(ids['pybullet_id'])
+        velocities_linear.append(vel_linear)
+        velocities_rotation.append(vel_rot)
+    return velocities_linear, velocities_rotation
 
 # # # # # # # # # # # # # # # # # # # # # # # # #
 if os.path.isdir(opt.outf):
@@ -45,7 +97,7 @@ camera = nvisii.entity.create(
 camera.get_transform().look_at(
     at = (0,0,0),
     up = (0,0,1),
-    eye = (4, 0, 2), #(4, 0, 6), #(10,0,4),
+    eye = (2, 0, 4), #(4, 0, 6), #(10,0,4),
 )
 nvisii.set_camera_entity(camera)
 
@@ -81,9 +133,24 @@ floor = nvisii.entity.create(
     material = nvisii.material.create("floor")
 )
 floor.get_transform().set_position((0,0,0))
-floor.get_transform().set_scale((10, 10, 10))
+floor.get_transform().set_scale((4, 4, 4)) #10, 10, 10
 floor.get_material().set_roughness(0.1)
 floor.get_material().set_base_color((0.8, 0.87, 0.88)) #(0.5,0.5,0.5)
+
+floor_colors = np.array([
+    [139,90,43],
+    [255,165,79],
+    [160,82,45],
+    [205,133,0],
+    [139,69,19],
+    [204,222,224]
+    ]) / 255.
+floor_textures = []
+texture_files = ["Wood_BaseColor.jpg", "WoodFloor_BaseColor.jpg", "WoodPanel.png"]
+for i, tf in enumerate(texture_files):
+    tex = nvisii.texture.create_from_file("tex-%d"%i, os.path.join("texture/", tf))
+    floor_tex = nvisii.texture.create_hsv("floor-%d"%i, tex, hue=0, saturation=.5, value=1.0, mix=1.0)
+    floor_textures.append((tex, floor_tex))
 
 # Set the collision with the floor mesh
 # first lets get the vertices 
@@ -114,8 +181,7 @@ p.createMultiBody(
 # lets create a bunch of objects 
 object_path = '/home/gun/Desktop/pybullet-URDF-models/urdf_models/models'
 object_names = [m for m in os.listdir(object_path) if os.path.isdir(os.path.join(object_path, m))]
-object_selected = np.random.choice(np.arange(len(object_names)), opt.nb_objects, replace=False)
-object_names = np.array(object_names)[object_selected]
+object_names = np.random.choice(object_names, opt.nb_objects, replace=False)
 
 meshes = []
 list_vertices = []
@@ -137,11 +203,11 @@ mesh_indices = np.arange(len(meshes))
 #mesh_indices = np.random.choice(np.arange(len(meshes)), opt.nb_objects, replace=False)
 for i, idx in enumerate(mesh_indices):
     name = f"mesh_{i}"
-    obj= nvisii.entity.create(
-        name = name,
-        transform = nvisii.transform.create(name),
-        material = nvisii.material.create(name)
-    )
+    obj = nvisii.entity.create(
+                name = name,
+                transform = nvisii.transform.create(name),
+                material = nvisii.material.create(name)
+            )
     obj.set_mesh(meshes[idx])
 
     # transforms
@@ -150,12 +216,13 @@ for i, idx in enumerate(mesh_indices):
         random.uniform(-2, 2), #(-4,4),
         random.uniform(1, 1.2) #(2,5)
     )
-    rot = nvisii.normalize(nvisii.quat(
-        random.uniform(-1,1),
-        random.uniform(-1,1),
-        random.uniform(-1,1),
-        random.uniform(-1,1),
-    ))
+    rot = get_rotation(0.0, 0.0, 0.0)
+    #rot = nvisii.normalize(nvisii.quat(
+    #    random.uniform(-1,1),
+    #    random.uniform(-1,1),
+    #    random.uniform(-1,1),
+    #    random.uniform(-1,1),
+    #))
     s = random.uniform(3, 5) #(0.2,0.5)
     scale = (s,s,s)
 
@@ -229,32 +296,100 @@ for i, idx in enumerate(mesh_indices):
         else:
             obj_mat.set_roughness(random.uniform(0.9,1)) # default is 1  
 
+
+threshold_linear = 0.003
+threshold_rotation = 0.003
+pre_selected_objects = ids_pybullet_and_nvisii_names
+
+x = np.linspace(-1.5, 1.5, 7)
+y = np.linspace(-1.5, 1.5, 7)
+xx, yy = np.meshgrid(x, y, sparse=False)
+xx = xx.reshape(-1)
+yy = yy.reshape(-1)
+
 # Lets run the simulation for a few steps. 
 num_exist_frames = len([f for f in os.listdir(f"{opt.outf}") if '.png' in f])
-for i in range (int(opt.nb_frames)):
-    steps_per_frame = math.ceil( 1.0 / (seconds_per_step * frames_per_second) )
-    for j in range(steps_per_frame):
-        p.stepSimulation()
+for ns in range (int(opt.nb_scenes)):
+    # set objects #
+    selected_objects = np.random.choice(ids_pybullet_and_nvisii_names, opt.inscene_objects, replace=False)
+    for idx, obj in enumerate(pre_selected_objects):
+        pos_hidden = [xx[idx], yy[idx], -1]
+        set_object_pose(obj, pos_hidden, rot=[1, 0, 0, 0])
 
-    # Lets update the pose of the objects in nvisii 
+    for idx, obj in enumerate(selected_objects):
+        pos_sel = 2*(np.random.rand(3) - 0.5)
+        pos_sel[2] = 0.5
+        set_object_pose(obj, pos_sel, rot=[1, 0, 0, 0])
+
+    # set texture of objects #
     for ids in ids_pybullet_and_nvisii_names:
-
-        # get the pose of the objects
-        pos, rot = p.getBasePositionAndOrientation(ids['pybullet_id'])
-
         # get the nvisii entity for that object
         obj_entity = nvisii.entity.get(ids['nvisii_id'])
-        obj_entity.get_transform().set_position(pos)
+        # Material setting
+        rgb = colorsys.hsv_to_rgb(
+            random.uniform(0,1),
+            random.uniform(0.7,1),
+            random.uniform(0.7,1)
+        )
+        obj_entity.get_material().set_base_color(rgb)
 
-        # nvisii quat expects w as the first argument
-        obj_entity.get_transform().set_rotation(rot)
-    print(f'rendering frame {str(i).zfill(5)}/{str(opt.nb_frames).zfill(5)}')
-    nvisii.render_to_file(
-        width=int(opt.width), 
-        height=int(opt.height), 
-        samples_per_pixel=int(opt.spp),
-        file_path=f"{opt.outf}/{str(num_exist_frames + i).zfill(5)}.png"
-    )
+    roughness = random.uniform(0.1, 0.5)
+    floor.get_material().clear_base_color_texture()
+    floor.get_material().set_roughness(roughness)
+    floor_cidx = np.random.choice(len(floor_colors)+len(floor_textures))
+    if floor_cidx < len(floor_colors):
+        floor.get_material().set_base_color(floor_colors[floor_cidx])
+    else:
+        f_cidx = floor_cidx - len(floor_colors)
+        tex, floor_tex = floor_textures[f_cidx]
+        floor.get_material().set_base_color_texture(floor_tex)
+        floor.get_material().set_roughness_texture(tex)
+
+
+    for nf in range(int(opt.nb_frames)):
+        # set poses & rots #
+        # get frames #
+        # Lets update the pose of the objects in nvisii 
+        targets = np.random.choice(selected_objects, 1, replace=False)
+        for ids in targets: #ids_pybullet_and_nvisii_names:
+            flag_collision = True
+            while flag_collision:
+                # get the pose of the objects
+                pos, rot = p.getBasePositionAndOrientation(ids['pybullet_id'])
+                print(rot)
+                collisions_before = get_contact_objects()
+
+                pos_new = 2*(np.random.rand(3) - 0.5)
+                pos_new[2] = 0.5
+                set_object_pose(ids, pos_new, rot=[1, 0, 0, 0])
+                collisions_after = get_contact_objects()
+
+                collisions_new = collisions_after - collisions_before
+                if len(collisions_new) > 0:
+                    print('collision')
+                    flag_collision = True
+                else:
+                    flag_collision = False
+
+        #steps_per_frame = math.ceil( 1.0 / (seconds_per_step * frames_per_second) )
+        for j in range(2000):
+            p.stepSimulation()
+            vel_linear, vel_rot = get_velocity(selected_objects)
+            stop_linear = (np.linalg.norm(vel_linear) < threshold_linear)
+            stop_rotation = (np.linalg.norm(vel_rot) < threshold_rotation)
+            if j%10==0:
+                if stop_linear and stop_rotation:
+                    break
+        sync_object_poses(selected_objects)
+
+        print(f'rendering frame {str(i).zfill(5)}/{str(opt.nb_frames).zfill(5)}')
+        nvisii.render_to_file(
+            width=int(opt.width), 
+            height=int(opt.height), 
+            samples_per_pixel=int(opt.spp),
+            file_path=f"{opt.outf}/{str(num_exist_frames + ns * opt.nb_frames + nf).zfill(5)}.png"
+        )
+    pre_selected_objects = selected_objects
 
 p.disconnect()
 nvisii.deinitialize()
