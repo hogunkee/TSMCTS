99ff6271882ec72443ca34e7217a98e53ef34f5e
hogun-mac <hogunhogun@naver.com>
2023-12-03 17:47:14 +0900

    merge data

--- tidying_line_diffuser/datasets/merge_data.py
+++ tidying_line_diffuser/datasets/merge_data.py
@@ -4,27 +4,32 @@ import numpy as np
 from PIL import Image
 from tqdm import tqdm
 
-res = 128
+res = 16 #128
 image_path = '/home/gun/ssd/disk/ur5_tidying_data/pybullet_single_bg/images/'
 out_path = '../data/'
-rgb_list = [f for f in os.listdir(image_path) if f.startswith('rgb_') and f.endswith('.png')]
-seg_list = [f for f in os.listdir(image_path) if f.startswith('seg_') and f.endswith('.npy')]
 
-rgb_npy_list = []
-for _r in tqdm(rgb_list):
-    rgb = np.array(Image.open(os.path.join(image_path, _r)))[:, :, :3]
-    rgb_resized = cv2.resize(rgb, (res, res), interpolation=cv2.INTER_AREA)
-    rgb_npy_list.append(np.expand_dims(rgb_resized, 0))
-rgb_concat = np.concatenate(rgb_npy_list)
-print('save rgb data:', rgb_concat.shape)
-np.save(os.path.join(out_path, 'rgb.npy'), rgb_concat)
+merge_rgb_data = False
+merge_segmentation_data = True
 
-seg_npy_list = []
-for _s in tqdm(seg_list):
-    segmap = np.load(os.path.join(image_path, _s))
-    segmap = np.flip(segmap, 0)
-    segmap_resized = cv2.resize(segmap, (res, res), interpolation=cv2.INTER_NEAREST)
-    seg_npy_list.append(segmap_resized)
-seg_concat = np.concatenate(np.expand_dims(seg_npy_list, 0))
-print('save segmentation data:', seg_concat.shape)
-np.save(os.path.join(out_path, 'segmap.npy'), seg_concat)
+if merge_rgb_data:
+    rgb_list = [f for f in os.listdir(image_path) if f.startswith('rgb_') and f.endswith('.png')]
+    rgb_npy_list = []
+    for _r in tqdm(rgb_list):
+        rgb = np.array(Image.open(os.path.join(image_path, _r)))[:, :, :3]
+        rgb_resized = cv2.resize(rgb, (res, res), interpolation=cv2.INTER_AREA)
+        rgb_npy_list.append(np.expand_dims(rgb_resized, 0))
+    rgb_concat = np.concatenate(rgb_npy_list)
+    print('save rgb data:', rgb_concat.shape)
+    np.save(os.path.join(out_path, 'rgb_%d.npy'%res), rgb_concat)
+
+if merge_segmentation_data:
+    seg_list = [f for f in os.listdir(image_path) if f.startswith('seg_') and f.endswith('.npy')]
+    seg_npy_list = []
+    for _s in tqdm(seg_list):
+        segmap = np.load(os.path.join(image_path, _s))
+        segmap = np.flip(segmap, 0)
+        segmap_resized = cv2.resize(segmap, (res, res), interpolation=cv2.INTER_NEAREST)
+        seg_npy_list.append(segmap_resized)
+    seg_concat = np.concatenate(np.expand_dims(seg_npy_list, 0))
+    print('save segmentation data:', seg_concat.shape)
+    np.save(os.path.join(out_path, 'segmap_%d.npy'%res), seg_concat)
