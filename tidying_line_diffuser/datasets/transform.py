b1ae20e1a7481d770e2a2dc0dc3d28cac62d58a7
hogun-mac <hogunhogun@naver.com>
2023-12-11 19:16:24 +0900

    use_color_jitter

--- tidying_line_diffuser/datasets/transform.py
+++ tidying_line_diffuser/datasets/transform.py
@@ -5,7 +5,8 @@ from torchvision.transforms import ColorJitter
 
 
 class Transform:
-    def __init__(self, max_translate=6, brightness=0.1, contrast=0.1, hue=0.02):
+    def __init__(self, use_color_jitter=True, max_translate=6, brightness=0.1, contrast=0.1, hue=0.02):
+        self.use_color_jitter = use_color_jitter
         self.color_jitter = ColorJitter(
             brightness=brightness,
             contrast=contrast,
@@ -14,6 +15,9 @@ class Transform:
         self.max_translate = max_translate
 
     def __call__(self, image):
-        image_aug = self.color_jitter(image)
+        if self.use_color_jitter:
+            image_aug = self.color_jitter(image)
+        else:
+            image_aug = image
         image_aug = 2. * image_aug / 255. - 1.
         return image_aug
