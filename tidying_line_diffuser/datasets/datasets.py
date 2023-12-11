b09c5956540c3ea2da32069e726b19731d827df5
hogun-mac <hogunhogun@naver.com>
2023-12-11 18:13:35 +0900

    target condition dataset with nobg

--- tidying_line_diffuser/datasets/datasets.py
+++ tidying_line_diffuser/datasets/datasets.py
@@ -63,6 +63,34 @@ class CondDiffusionDatasetNoBG(Dataset):
 
 
 class TargetCondDiffusionDataset(Dataset):
+    def __init__(self, rgb, segmap, resolution=128, cond_resolution=16, num_duplication=4):
+        self.rgb = np.reshape(rgb, (-1, resolution, resolution, 3))
+        self.segmap = np.reshape(segmap, (-1, cond_resolution, cond_resolution))
+        self.preprocess()
+
+        self.num_duplication = num_duplication
+        self.hash_augment = self.get_augmentation()
+        self.fsize = len(self.hash_augment) * int(len(self.rgb) / num_duplication)
+
+    def get_augmentation(self):
+        num_duplication = self.num_duplication
+        hash_list = []
+        for i in range(num_duplication):
+            for j in range(num_duplication):
+                hash_list.append([i, j])
+        return np.array(hash_list)
+
+    def __len__(self):
+        return self.fsize #len(self.rgb)
+
+    def __getitem__(self, item):
+        idx1 = item // len(self.hash_augment)
+        idx2 = item % len(self.hash_augment)
+        source_idx, target_idx = self.num_duplication * idx1 + self.hash_augment[idx2]
+        return self.rgb[source_idx], self.rgb[target_idx], self.segmap[target_idx]
+
+
+class TargetCondDiffusionDatasetNoBG(Dataset):
     def __init__(self, rgb, segmap, mask, resolution=128, cond_resolution=16, num_duplication=4):
         self.rgb = np.reshape(rgb, (-1, resolution, resolution, 3))
         self.mask = np.reshape(mask, (-1, resolution, resolution, 1))
