5930bdb2cae26faf3660c2d5e29dc26b3556307a
hogunkee <hogunhogun@naver.com>
2024-03-25 23:56:04 +0900

    measure

--- mcts/measure_metrics.py
+++ mcts/measure_metrics.py
@@ -4,6 +4,8 @@ import numpy as np
 def filter_log(log):
     success = False
     log = [float(t.replace('\n', '').split(' ')[-1]) for t in log if 'Score' in t]
+    if len(log)<2:
+        return None
     if log[-1]==log[-2]:
         if log[-1]>0.9:
             success = True
@@ -14,18 +16,26 @@ def filter_log(log):
 scenes = np.arange(40).tolist()
 logs = [d for d in os.listdir('data') if d.startswith('mcts') or d.startswith('alphago')]
 for logname in logs:
-    print(logname)
     ep_length = []
     ep_success_length = []
-    scenes = [s for s in os.listdir(os.path.join('data', logname)) is s.startswith('scene')]
+    scenes = [s for s in os.listdir(os.path.join('data', logname)) if s.startswith('scene')]
+    if len(scenes)<5:
+        continue
+    print(logname)
     for scene in scenes:
         scene_dir = os.path.join('data', logname, scene)
-        logfile = [f for f in os.listdir(scene_dir) if f.endswith('.log')][0]
+        logfile = [f for f in os.listdir(scene_dir) if f.endswith('.log')]
+        if len(logfile)==0:
+            continue
+        logfile = logfile[0]
         with open(os.path.join(scene_dir, logfile), 'r') as f:
             scores = filter_log(f.readlines())
+            if scores is None:
+                continue
         if scores[-1]:
             ep_success_length.append(len(scores[:-1]))
         ep_length.append(len(scores[:-1]))
+    print('Num episodes:', len(scenes))
     print('Success rate:', len(ep_success_length)/len(scenes))
     print('Success average length:', np.mean(ep_success_length))
     print('Average length:', np.mean(ep_length))
