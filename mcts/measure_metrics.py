1a874165e7a337f9404bb255fbac9be9b709edcc
hogunkee <hogunhogun@naver.com>
2024-03-26 09:52:08 +0900

    measure

--- mcts/measure_metrics.py
+++ mcts/measure_metrics.py
@@ -1,6 +1,17 @@
 import os
 import numpy as np
+from dateutil import parser
 
+def filter_log_time(log):
+    success = False
+    times = [parser.parse(log[i-1].split(',')[0]) for i in range(len(log)) if 'Best Action' in log[i]]
+    return times
+def get_intervals(times):
+    delta_time = []
+    for i in range(len(times)-1):
+        delta = times[i+1] - times[i]
+        delta_time.append(delta)
+    return delta_time
 def filter_log(log):
     success = False
     log = [float(t.replace('\n', '').split(' ')[-1]) for t in log if 'Score' in t]
@@ -18,6 +29,7 @@ logs = [d for d in os.listdir('data') if d.startswith('mcts') or d.startswith('a
 for logname in logs:
     ep_length = []
     ep_success_length = []
+    deltas = []
     scenes = [s for s in os.listdir(os.path.join('data', logname)) if s.startswith('scene')]
     if len(scenes)<5:
         continue
@@ -29,13 +41,17 @@ for logname in logs:
             continue
         logfile = logfile[0]
         with open(os.path.join(scene_dir, logfile), 'r') as f:
-            scores = filter_log(f.readlines())
-            if scores is None:
-                continue
+            x = f.readlines()
+        times = filter_log_time(x)
+        deltas += get_intervals(times)
+        scores = filter_log(x)
+        if scores is None:
+            continue
         if scores[-1]:
             ep_success_length.append(len(scores[:-1]))
         ep_length.append(len(scores[:-1]))
     print('Num episodes:', len(scenes))
+    print('Average time:', np.mean(deltas).seconds)
     print('Success rate:', len(ep_success_length)/len(scenes))
     print('Success average length:', np.mean(ep_success_length))
     print('Average length:', np.mean(ep_length))
