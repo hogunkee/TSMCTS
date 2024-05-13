ab16e3721f9ec96ecc64749f16f92d0198ec2c7c
hogunkee <hogunhogun@naver.com>
2024-05-13 20:39:58 +0900

    Evaluate structformer

---
+++ structformer/pc_utils.py
@@ -0,0 +1,29 @@
+import os
+import sys
+import numpy as np
+
+def depth2pc(depth, K, rgb=None):
+    """
+    Convert depth and intrinsics to point cloud and optionally point cloud color
+    :param depth: hxw depth map in m
+    :param K: 3x3 Camera Matrix with intrinsics
+    :returns: (Nx3 point cloud, point cloud color)
+    """
+
+    mask = np.where(depth > 0)
+    x,y = mask[1], mask[0]
+    
+    normalized_x = (x.astype(np.float32) - K[0,2])
+    normalized_y = (y.astype(np.float32) - K[1,2])
+
+    world_x = normalized_x * depth[y, x] / K[0,0]
+    world_y = normalized_y * depth[y, x] / K[1,1]
+    world_z = depth[y, x]
+
+    if rgb is not None:
+        rgb = rgb[y,x,:]
+        
+    pc = np.vstack((world_x, world_y, world_z)).T
+    return (pc, rgb)
+
+
