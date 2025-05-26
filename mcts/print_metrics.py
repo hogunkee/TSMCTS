44b2215632fc9b959aad94f4c32477482cc8ed1a
hogun-ryan <hogunhogun@naver.com>
2025-05-26 15:55:47 +0900

    print logs

--- mcts/print_metrics.py
+++ mcts/print_metrics.py
@@ -8,6 +8,7 @@ from argparse import ArgumentParser
 argparser = ArgumentParser()
 argparser.add_argument('--threshold', type=float, default=0.9)
 argparser.add_argument('--after', type=str, default=None)
+argparser.add_argument('--out', type=str, default=None)
 args = argparser.parse_args()
 
 def filter_log_time(log):
@@ -47,7 +48,8 @@ logs = sorted([d for d in os.listdir('data') if not d.startswith('classification
 #    logs = sorted([d for d in logs if d[-9:-5] >= args.after])
 print('Measure metrics for:', logs)
 
-df = pd.DataFrame(columns=['success', 'score', 'eplen', 'numep'])
+df = pd.DataFrame(columns=['success', 'score', 'eplen', 'numep', 'seed', 'logname', 'env'])
+error_files = []
 for logname in logs:
     envs = os.listdir(os.path.join('data', logname))
     for env in envs:
@@ -57,44 +59,48 @@ for logname in logs:
         try:
             with open(os.path.join('data', logname, env, 'config.json'), 'r') as f:
                 cfg = json.load(f)
+                seed = cfg['seed']
+            ep_length = []
+            ep_success_length = []
+            ep_success_score = []
+            deltas = []
+            for scene in scenes:
+                scene_dir = os.path.join('data', logname, env, scene)
+                logfile = [f for f in os.listdir(scene_dir) if f.endswith('.log')]
+                if len(logfile)==0:
+                    continue
+                logfile = logfile[0]
+                with open(os.path.join(scene_dir, logfile), 'r') as f:
+                    x = f.readlines()
+                times = filter_log_time(x)
+                deltas += get_intervals(times)
+                scores, success_score = filter_log(x)
+                if success_score !=0:
+                    ep_success_score.append(success_score)
+                if scores is None:
+                    continue
+                if scores[-1]:
+                    ep_success_length.append(len(scores[:-1]))
+                ep_length.append(len(scores[:-1]))
+
+            numep = len(scenes)
+            success = '%.2f'%(len(ep_success_length)/len(scenes))
+            score = '%.3f'%(np.mean(ep_success_score))
+            eplen = '%.3f'%(np.mean(ep_success_length))
+            df.loc['%s-%s'%(logname, env)] = [success, score, eplen, numep, seed, logname, env]
+
+            #print('Num episodes:', len(scenes))
+            #print('Average time:', np.mean(deltas).seconds)
+            #print('Success rate: %.3f' %(len(ep_success_length)/len(scenes)))
+            #if len(ep_success_score)!=0:
+            #    print('Average score: %.3f' %np.mean(ep_success_score))
+            #print('Episode length: %.3f' %np.mean(ep_success_length))
+            #print('Average length:', np.mean(ep_length))
+            #print('-'*40)
         except:
+            error_files.append(os.path.join(logname, env, scene))
             continue
-
-        ep_length = []
-        ep_success_length = []
-        ep_success_score = []
-        deltas = []
-        for scene in scenes:
-            scene_dir = os.path.join('data', logname, env, scene)
-            logfile = [f for f in os.listdir(scene_dir) if f.endswith('.log')]
-            if len(logfile)==0:
-                continue
-            logfile = logfile[0]
-            with open(os.path.join(scene_dir, logfile), 'r') as f:
-                x = f.readlines()
-            times = filter_log_time(x)
-            deltas += get_intervals(times)
-            scores, success_score = filter_log(x)
-            if success_score !=0:
-                ep_success_score.append(success_score)
-            if scores is None:
-                continue
-            if scores[-1]:
-                ep_success_length.append(len(scores[:-1]))
-            ep_length.append(len(scores[:-1]))
-
-        numep = len(scenes)
-        success = '%.2f'%(len(ep_success_length)/len(scenes))
-        score = '%.3f'%(np.mean(ep_success_score))
-        eplen = '%.3f'%(np.mean(ep_success_length))
-        df['%s-%s'%(log, env)] = [success, score, eplen, numep]
-
-        #print('Num episodes:', len(scenes))
-        #print('Average time:', np.mean(deltas).seconds)
-        #print('Success rate: %.3f' %(len(ep_success_length)/len(scenes)))
-        #if len(ep_success_score)!=0:
-        #    print('Average score: %.3f' %np.mean(ep_success_score))
-        #print('Episode length: %.3f' %np.mean(ep_success_length))
-        #print('Average length:', np.mean(ep_length))
-        #print('-'*40)
+if args.out is not None:
+    df.to_csv(args.out, index=False)
 print(df)
+print('errors:', error_files)
